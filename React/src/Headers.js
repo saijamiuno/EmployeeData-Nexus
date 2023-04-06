3c84ee881e1dac7946eb6bb85fd5f369083dbdfc
Ksishore <satyakishore04@gmail.com>
2023-04-06 17:28:09 +0530

    HomePage

--- React/src/Headers.js
+++ React/src/Headers.js
@@ -3,61 +3,51 @@ import React from "react";
 import { Col, Row, Button } from "antd";
 import { Link } from "react-router-dom";
 export default function Headers(props) {
-  
   return (
     <Col span={24}>
-        <div style={{ backgroundColor: "#13c2c2",height:"60px"}}>
-      <Row style={{ justifyContent: "right" }}>
-       
-        
-      <Col span={2} id = 'nav-links-container'>
-            
+      <div style={{ backgroundColor: "#13c2c2", height: "60px" }}>
+        <Row style={{ justifyContent: "right" }}>
+          <Col span={2} id="nav-links-container">
             <Button
               style={{
                 backgroundColor: "#13c2c2",
                 color: "#000",
                 border: "0px",
-                margin:"1rem"
+                margin: "1rem",
               }}
-              onClick={()=>window.location.href="/dashboard"}
+              onClick={() => (window.location.href = "/dashboard")}
             >
               Dashboard
             </Button>
           </Col>
-            <Col span={2} id = 'nav-links-container'>
-            
-              <Button
-                style={{
-                  backgroundColor: "#13c2c2",
-                  color: "#000",
-                  border: "0px",
-                  margin:"1rem"
-                }}
-                onClick={()=>window.location.href="/"}
-              >
-                HOME
-              </Button>
-            </Col>
-            <Col span={2}>
-              <Button
-                style={{
-                  backgroundColor: "#13c2c2",
-                  color: "#000",
-                  border: "0px",
-                  margin:"1rem"
-                }}
-                onClick={()=>window.location.href="/usersTable"}
-              >
-                USERS
-              </Button>
-            </Col>
-             
-            
-         
-       
-      </Row>
-    </div>
+          <Col span={2} id="nav-links-container">
+            <Button
+              style={{
+                backgroundColor: "#13c2c2",
+                color: "#000",
+                border: "0px",
+                margin: "1rem",
+              }}
+              onClick={() => (window.location.href = "/")}
+            >
+              HOMEPAGE
+            </Button>
+          </Col>
+          <Col span={2}>
+            <Button
+              style={{
+                backgroundColor: "#13c2c2",
+                color: "#000",
+                border: "0px",
+                margin: "1rem",
+              }}
+              onClick={() => (window.location.href = "/usersTable")}
+            >
+              USERS
+            </Button>
+          </Col>
+        </Row>
+      </div>
     </Col>
-  
   );
 }
