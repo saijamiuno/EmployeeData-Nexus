f0aaa56b2df22f1ecb6aa4ee81c7275b8ed06a27
Saijami38 <saijami38.com>
2023-04-07 20:48:35 +0530

    Details Page

--- React/src/routes/UserDetails.js
+++ React/src/routes/UserDetails.js
@@ -1,16 +1,33 @@
-import React from "react";
-import { Layout, Input, Menu, Row, Col, Button, Table } from "antd";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useParams } from "react-router-dom";
+import "../App.css"
 
 export default function UserDetails(props) {
-  const { Header, Footer, Sider, Content } = Layout;
+  const { id } = useParams();
+  const [user, setUser] = useState({});
 
-  const getUserDetails = async () => {
-    let _id = props?.params?.id;
-    console.log(_id,"idsss");
-    const { data } = await axios.get("/getUserDetails/" + `${_id}`).then(response => (console.log(response,"respo")));
-    
-  };
+  useEffect(() => {
+    axios
+      .get(`/getUserDetails/${id}`)
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-  return <><Table/></>;
+  return (
+    <div>
+      <h1>User Details</h1>
+      <p>First Name: {user.firstName}</p>
+      <p>Last Name: {user.lastName}</p>
+      <p>Phone Number: {user.phone}</p>
+      <p>Email: {user.email}</p>
+      <p>Date Of Birth: {user.dob}</p>
+      <p>Designation: {user.designation}</p>
+      <p>Comments: {user.comments}</p>
+    </div>
+  );
 }
