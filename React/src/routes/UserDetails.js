f0658f3c9bc35aebbf2df9c4e3c5acebdf399d56
Saijami38 <saijami38.com>
2023-04-07 21:17:53 +0530

    Back Button Added

--- React/src/routes/UserDetails.js
+++ React/src/routes/UserDetails.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Col, Row, Button } from "antd";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import "../App.css";
@@ -19,30 +20,48 @@ export default function UserDetails(props) {
   }, []);
 
   return (
-    <div className="user-details-container">
-      <div className="user-details-card">
-        <div className="card-body">
-          <h1 className="card-title">User Details</h1>
-          <p>
-            <strong>Name:</strong> {user.firstName} {user.lastName}
-          </p>
-          <p>
-            <strong>Email:</strong> {user.email}
-          </p>
-          <p>
-            <strong>Phone:</strong> {user.phone}
-          </p>
-          <p>
-            <strong>Date of Birth:</strong> {user.dob}
-          </p>
-          <p>
-            <strong>Designation:</strong> {user.designation}
-          </p>
-          <p>
-            <strong>Comments:</strong> {user.comments}
-          </p>
-        </div>
-      </div>
-    </div>
+    <Col span={24}>
+      <Row>
+        <Col span={22}>
+          <div className="user-details-container">
+            <div className="user-details-card">
+              <div className="card-body">
+                <h1 className="card-title">User Details</h1>
+                <p>
+                  <strong>Name:</strong> {user.firstName} {user.lastName}
+                </p>
+                <p>
+                  <strong>Email:</strong> {user.email}
+                </p>
+                <p>
+                  <strong>Phone:</strong> {user.phone}
+                </p>
+                <p>
+                  <strong>Date of Birth:</strong> {user.dob}
+                </p>
+                <p>
+                  <strong>Designation:</strong> {user.designation}
+                </p>
+                <p>
+                  <strong>Comments:</strong> {user.comments}
+                </p>
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col span={2}>
+          <Button
+            style={{
+              backgroundColor: "#0958d9",
+              color: "#fff",
+              marginTop: "12px",
+            }}
+            onClick={() => window.history.back(-1)}
+          >
+            BACK
+          </Button>
+        </Col>
+      </Row>
+    </Col>
   );
 }
