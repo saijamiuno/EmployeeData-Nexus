6d4cccf4cf30de8d7a2ba5561a6bbdeac4e14e6b
saijami <saijami@unosimple.com>
2023-04-13 17:23:55 +0530

    Login Img chages

--- React/src/Login.js
+++ React/src/Login.js
@@ -1,6 +1,7 @@
 import { Col, Row, Button, Checkbox, Form, Input, Card } from "antd";
 import { useState } from "react";
-// import './Login.css';
+import "./App.css";
+import login from "./Images/login.jpg";
 
 function Login() {
   const onFinish = (values) => {
@@ -10,65 +11,79 @@ function Login() {
     console.log("Failed:", errorInfo);
   };
   return (
-    <Col span={24}>
-      <Row>
-        <Col span={10}></Col>
-        <Col span={5} style={{ marginTop: "35vh" }}>
-          <Card style={{ boxShadow: "  0 4px 8px 0 rgba(0, 0, 0, 0.2)" }}>
-            <span style={{ marginLeft: "13vh", fontSize: "30px" }}>LOGIN</span>
+    <div
+      className="body"
+      style={{
+        backgroundImage: `url(${login})`,
+        backgroundSize: "cover",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Col span={24}>
+        <Row>
+          <Col span={10}></Col>
+          <Col span={5}>
+            <Card style={{ boxShadow: "0 4px 8px 0 rgba(0, 0, 8, 0.2)" }}>
+              <span style={{ marginLeft: "13vh", fontSize: "30px" }}>
+                LOGIN
+              </span>
 
-            <Form
-              name="basic"
-              initialValues={{
-                remember: true,
-              }}
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
-              autoComplete="off"
-            >
-              <span style={{ fontSize: "20px" }}>Username</span>
-              <Form.Item
-                name="username"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your username!",
-                  },
-                ]}
+              <Form
+                name="basic"
+                initialValues={{
+                  remember: true,
+                }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
               >
-                <Input />
-              </Form.Item>
-              <span style={{ fontSize: "20px" }}>Password</span>
-              <Form.Item
-                name="password"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your password!",
-                  },
-                ]}
-              >
-                <Input.Password />
-              </Form.Item>
-
-              <Form.Item>
-                <Button
-                  onClick={() => (window.location.href = "/homePage")}
-                  style={{
-                    width: "100%",
-                    backgroundColor: "#0958d9",
-                    color: "#fff",
-                  }}
-                  htmlType="submit"
+                <span style={{ fontSize: "20px" }}>Username</span>
+                <Form.Item
+                  name="username"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your username!",
+                    },
+                  ]}
                 >
-                  Submit
-                </Button>
-              </Form.Item>
-            </Form>
-          </Card>
-        </Col>
-      </Row>
-    </Col>
+                  <Input />
+                </Form.Item>
+                <span style={{ fontSize: "20px" }}>Password</span>
+                <Form.Item
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your password!",
+                    },
+                  ]}
+                >
+                  <Input.Password />
+                </Form.Item>
+
+                <Form.Item>
+                  <Button
+                    onClick={() => (window.location.href = "/homePage")}
+                    style={{
+                      width: "100%",
+                      backgroundColor: "#0958d9",
+                      color: "#fff",
+                    }}
+                    htmlType="submit"
+                  >
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
+      </Col>
+    </div>
   );
 }
 
